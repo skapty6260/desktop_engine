34b28eb62469b66ebec0cd1bb412577325114fdc
skapty6260 <skaptysmail@bk.ru>
2025-11-14 18:21:16 +0300

    stdio fix

--- desktop_engine.windows_server/src/main.c
+++ desktop_engine.windows_server/src/main.c
@@ -3,6 +3,7 @@ Basic wayland server (Open clients, create surfaces)
 Logger (Should work with logfiles, ipc logging)
 Ipc bridge (Should send clients info, should receive input events)
 */
+#include <stdio.h>
 #include <server.h>
 
 int main(int argc, char **argv) {
