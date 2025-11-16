92524cf3cce83327a885279aa9748db69f5a372f
skapty6260 <skaptysmail@bk.ru>
2025-11-16 18:52:44 +0300

    Wayland server #19 typo fix

--- desktop_engine.windows_server/src/server.h
+++ desktop_engine.windows_server/src/server.h
@@ -21,7 +21,7 @@ struct client_state {
 };
 
 typedef struct server_config {
-    const char* statup_cmd;
+    const char* startup_cmd;
 } server_config_t;
 
 void server_init(struct server *server);
