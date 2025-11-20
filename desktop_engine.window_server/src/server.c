8d2a52b87a341e47d10498a191a2543003a0f711
skapty6260 <skaptysmail@bk.ru>
2025-11-20 04:33:18 +0300

    compositor version 4

--- desktop_engine.window_server/src/server.c
+++ desktop_engine.window_server/src/server.c
@@ -107,7 +107,7 @@ void server_init(struct server *server) {
     server->compositor_global = wl_global_create(
         server->display, 
         &wl_compositor_interface, 
-        1, server, bind_compositor
+        4, server, bind_compositor
     );
 
     if (!server->xdg_wm_base_global || !server->shm_global || !server->compositor_global) {
