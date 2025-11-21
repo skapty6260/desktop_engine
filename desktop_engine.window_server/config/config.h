e571b7de09105cee4f43b5f8c9c0813e7d00b3f0
skapty6260 <skaptysmail@bk.ru>
2025-11-21 19:55:24 +0300

    import fix #3

--- desktop_engine.window_server/config/config.h
+++ desktop_engine.window_server/config/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 #include "logger/logger.h"
-#include "src/server.h"
+#include "src/server/server.h"
 
 void parse_args(int argc, char **argv, logger_config_t *logger_config, server_config_t *server_config);
 void load_default_config(logger_config_t* logger_conf, server_config_t* server_config);
