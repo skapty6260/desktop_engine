a17cbb426119b0d707e4a01180cd32c304ec3487
skapty6260 <skaptysmail@bk.ru>
2025-11-24 04:06:52 +0300

    debug surface version

--- desktop_engine.wayland_server/src/server/compositor.c
+++ desktop_engine.wayland_server/src/server/compositor.c
@@ -313,7 +313,7 @@ static void compositor_create_surface(struct wl_client *client, struct wl_resour
     
     wl_resource_set_implementation(surface_resource, &surface_implementation, surface, NULL);
     
-    SERVER_DEBUG("COMPOSITOR: Surface created successfully, resource=%p, implementation set, resource version: %i", surface_resource, wl_resource_get_version(resource));
+    SERVER_DEBUG("COMPOSITOR: Surface created successfully, resource=%p, implementation set, resource version: %i", surface_resource, wl_resource_get_version(surface_resource));
 }
 
 //  WL_COMPOSITOR create_region
