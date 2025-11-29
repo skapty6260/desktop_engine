518938bb8be8058bedc5977bf3ceb917dbd13422
skapty6260 <skaptysmail@bk.ru>
2025-11-29 13:10:48 +0300

    buffer->stride -> buffer->shm.shm_buffer.stride

--- desktop_engine.wayland_server/src/server/compositor.c
+++ desktop_engine.wayland_server/src/server/compositor.c
@@ -137,7 +137,7 @@ static struct buffer *custom_buffer_from_resource(struct wl_resource *resource) 
         // мы можем либо создать обертку, либо работать напрямую с данными
         SERVER_DEBUG("Custom SHM buffer: %dx%d, format=0x%x, stride=%d",
                     buffer->width, buffer->height, 
-                    custom_shm_buffer->format, custom_shm_buffer->stride);
+                    custom_shm_buffer->shm.shm_buffer.format, custom_shm_buffer->shm.shm_buffer.stride);
         return buffer;
     }
 
