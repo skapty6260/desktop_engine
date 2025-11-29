0c78b905cf50a9fe928c45e611e48949cbaebdec
skapty6260 <skaptysmail@bk.ru>
2025-11-30 01:24:03 +0300

    fix wrong param

--- desktop_engine.wayland_server/src/server/wayland/compositor.c
+++ desktop_engine.wayland_server/src/server/wayland/compositor.c
@@ -66,7 +66,7 @@ void surface_headless_attach(struct wl_client *client, struct wl_resource *resou
     if (!surface || !buffer_resource) return;
     
     struct buffer *buffer = wl_resource_get_user_data(resource);
-    SERVER_DEBUG("Called attach buffer with type: %s, size: %i or %iX%i", buffer_type_to_string(buffer->type), buffer->size, buffer->width, buffer->height);
+    SERVER_DEBUG("Called attach buffer with type: %s, size: %i or %iX%i", buffer_type_to_string(buffer), buffer->size, buffer->width, buffer->height);
 }
 
 static void compositor_create_surface(struct wl_client *client, struct wl_resource *resource, uint32_t id) {
