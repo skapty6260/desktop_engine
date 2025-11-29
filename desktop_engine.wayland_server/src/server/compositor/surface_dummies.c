df79e679b149403c423f77e1c1f8f4ee6eca977b
skapty6260 <skaptysmail@bk.ru>
2025-11-29 14:19:30 +0300

    fix header problems

--- desktop_engine.wayland_server/src/server/compositor/surface_dummies.c
+++ desktop_engine.wayland_server/src/server/compositor/surface_dummies.c
@@ -3,7 +3,7 @@
 #include "../../logger/logger.h"
 #include <stdlib.h>
 
-void surface_destroy(struct wl_client *client, struct wl_resource *resource) {
+static void surface_destroy(struct wl_client *client, struct wl_resource *resource) {
     struct surface *surface = wl_resource_get_user_data(resource);
     
     SERVER_DEBUG("SURFACE: Resource destroyed, surface=%p", surface);
@@ -14,23 +14,23 @@ void surface_destroy(struct wl_client *client, struct wl_resource *resource) {
     }
 }
 
-void surface_damage(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y, int32_t width, int32_t height) {
+static void surface_damage(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y, int32_t width, int32_t height) {
     SERVER_DEBUG("SURFACE DAMAGE CALLED");
 }
 
-void surface_frame(struct wl_client *client, struct wl_resource *resource, uint32_t callback) {
+static void surface_frame(struct wl_client *client, struct wl_resource *resource, uint32_t callback) {
     SERVER_DEBUG("SURFACE FRAME CALLED");
 }
 
-void surface_set_opaque_region(struct wl_client *client, struct wl_resource *resource, struct wl_resource *region) {
+static void surface_set_opaque_region(struct wl_client *client, struct wl_resource *resource, struct wl_resource *region) {
     SERVER_DEBUG("SURFACE SET_OPAQUE_REGION CALLED");
 }
 
-void surface_set_input_region(struct wl_client *client, struct wl_resource *resource, struct wl_resource *region) {
+static void surface_set_input_region(struct wl_client *client, struct wl_resource *resource, struct wl_resource *region) {
     SERVER_DEBUG("SURFACE SET_INPUT_REGION CALLED");
 }
 
-void surface_commit(struct wl_client *client, struct wl_resource *resource) {
+static void surface_commit(struct wl_client *client, struct wl_resource *resource) {
     // struct surface *surface = wl_resource_get_user_data(resource);
 
     // if (!surface) return;
@@ -65,19 +65,32 @@ void surface_commit(struct wl_client *client, struct wl_resource *resource) {
     SERVER_DEBUG("SURFACE COMMIT CALLED");
 }
 
-void surface_set_buffer_transform(struct wl_client *client, struct wl_resource *resource, int32_t transform) {
+static void surface_set_buffer_transform(struct wl_client *client, struct wl_resource *resource, int32_t transform) {
     SERVER_DEBUG("SURFACE SET_BUFFER_TRANSFORM CALLED");
 }
 
-void surface_set_buffer_scale(struct wl_client *client, struct wl_resource *resource, int32_t scale) {
+static void surface_set_buffer_scale(struct wl_client *client, struct wl_resource *resource, int32_t scale) {
     SERVER_DEBUG("SURFACE SET_BUFFER_SCALE CALLED");
 }
 
-void surface_damage_buffer(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y, int32_t width, int32_t height) {
+static void surface_damage_buffer(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y, int32_t width, int32_t height) {
     SERVER_DEBUG("SURFACE DAMAGE BUFFER CALLED");
 }
 
-void surface_offset(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y) {
+static void surface_offset(struct wl_client *client, struct wl_resource *resource, int32_t x, int32_t y) {
     SERVER_DEBUG("SURFACE OFFSET CALLED");
 }
 
+const struct wl_surface_interface surface_implementation = {
+    .destroy = surface_destroy,
+    .attach = surface_headless_attach,
+    .damage = surface_damage,
+    .frame = surface_frame,
+    .set_opaque_region = surface_set_opaque_region,
+    .set_input_region = surface_set_input_region,
+    .commit = surface_commit,
+    .set_buffer_transform = surface_set_buffer_transform,
+    .set_buffer_scale = surface_set_buffer_scale,
+    .damage_buffer = surface_damage_buffer,
+    .offset = surface_offset,
+};
