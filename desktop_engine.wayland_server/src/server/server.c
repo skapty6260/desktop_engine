401166228ef1cc58ad8e400685bbcd75216eb136
skapty6260 <skaptysmail@bk.ru>
2025-12-03 16:34:56 +0300

    tes

--- desktop_engine.wayland_server/src/server/server.c
+++ desktop_engine.wayland_server/src/server/server.c
@@ -50,38 +50,24 @@ void server_run(struct server *server) {
     wl_display_run(server->display);
 }
 
-/* Макрос для удобства использования offsetof */
-#define CLEANUP_WL_LIST(list, type, callback) \
-    wl_list_cleanup_generic(list, offsetof(type, link), \
-                           (void (*)(void *))callback)
-
-/* Базовая функция */
-static inline void wl_list_cleanup_generic(
-    struct wl_list *list,
-    size_t link_offset,
-    void (*callback)(void *)
-) {
-    struct wl_list *pos, *tmp;
-    wl_list_for_each_safe(pos, tmp, list, link) {
-        wl_list_remove(pos);
-        void *item = (char *)pos - link_offset;
-        
-        if (callback) {
-            callback(item);
-        }
-        
-        free(item);
-    }
-}
+#define CLEANUP_WL_LIST(type, list, callback) \
+    do { \
+        struct type *pos, *tmp; \
+        wl_list_for_each_safe(pos, tmp, list, link) { \
+            wl_list_remove(&pos->link); \
+            if (callback) callback(pos); \
+            free(pos); \
+        } \
+    } while(0)
 
 void server_cleanup(struct server *server) {
     wl_display_destroy_clients(server->display);
 
     /* Cleanup surfaces */
-    CLEANUP_WL_LIST(&server->surfaces, struct surface, NULL);
+    CLEANUP_WL_LIST(surface, &server->surfaces, NULL);
 
     /* Cleanup shm_pools */
-    CLEANUP_WL_LIST(&server->shm_pools, struct shm_pool, destroy_shm_pool);
+    CLEANUP_WL_LIST(shm_pool, &server->shm_pools, destroy_shm_pool);
 
     SERVER_DEBUG("Destroying wl display");
     if (server->display) {
