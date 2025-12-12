4f00591b6e24548df53f8d438388f035298f2042
skapty6260 <skaptysmail@bk.ru>
2025-12-12 06:56:10 +0300

    Fix ! to be != 0

--- desktop_engine.wayland_server/src/main.c
+++ desktop_engine.wayland_server/src/main.c
@@ -73,7 +73,7 @@ int main(int argc, char **argv) {
         EXIT_AND_ERROR("Failed to create dbus server");
     }
 
-    if (!dbus_start_main_loop(dbus_server)) {
+    if (dbus_start_main_loop(dbus_server) != 0) {
         EXIT_AND_ERROR("Failed to run dbus main loop in different thread");
     }
     // struct dbus_wayland_integration_data *dbus_wayland_integration_data = dbus_wayland_integration_create(server.display);
